19f2d5a474d9958bcf14fef5740cbfc563384c97
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-08-27 12:47:14 -0700

    fixed error in dashboard

--- frontend/src/components/DashBoard/DashBoard.jsx
+++ frontend/src/components/DashBoard/DashBoard.jsx
@@ -1,114 +1,40 @@
-import { useEffect, useState } from "react";
-import { useParams, Link, NavLink } from "react-router-dom";
-// import Profile from "../Profile/Profile";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchAccountProfile,
-  fetchDeleteAction,
-  fetchDeleteContact,
-} from "../../store/accounts";
-import { fetchDeleteOrder } from "../../store/orders";
-import "./AccountProfile.scss";
+import { NavLink } from "react-router-dom";
+import FilterAccounts from "../FilterAccounts/FilterAccounts";
+import { fetchUserAccounts } from "../../store/accounts";
+import "./DashBoard.scss";
 
-export default function AccountProfile() {
-  const { id } = useParams();
+// Dashboard
+export default function DashBoard() {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
-  // const navigate = useNavigate()
-  const [profile, setProfile] = useState({});
-  const [isWeekFilter, setIsWeekFilter] = useState(true);
-  // const accounts = useSelector((state) => state.accounts);
   const user = useSelector((state) => state.session.user);
-  const acctId = parseInt(id);
+  const accounts = useSelector((state) => state.accounts);
+  const [isWeekFilter, setIsWeekFilter] = useState(true);
+
+  const usersActions = Object.values(accounts).flatMap(
+    (account) => account.actions || []
+  );
 
   useEffect(() => {
-    if (user) {
-      dispatch(fetchAccountProfile(acctId))
-        .then((response) => {
-          if (response) {
-            setProfile(response);
-          }
-        })
-        .catch((error) => {
-          console.error("Failed to fetch account profile:", error);
-        });
-    }
-  }, [dispatch, acctId, user]);
+    dispatch(fetchUserAccounts()).catch(setError);
+  }, [dispatch]);
 
-  if (!profile || !user) {
-    return <div>Loading...</div>;
-  }
-
-  let usersActions;
-  let accountContacts;
-
-  if (profile.actions && profile.contacts) {
-    usersActions = profile.actions.flatMap((action) => action);
-    accountContacts = profile.contacts.flatMap((contact) => contact);
-  }
-
-  //  Optimistic UI Update: After the deletion request, the local profile state is immediately updated to remove the deleted action, without waiting for a full refetch.
-  //  This method does not require refetching the entire profile or actions list, making the UI more efficient and responsive.
-  const handleRemoveContact = async (e, contactId) => {
-    e.preventDefault();
-    try {
-      await dispatch(fetchDeleteContact(contactId));
-      setProfile((prevProfile) => ({
-        ...prevProfile,
-        contacts: prevProfile.actions.filter(
-          (contact) => contact.id !== contactId
-        ),
-      }));
-    } catch (err) {
-      console.error("Failed to delete contact:", err);
-    }
-  };
-
-  function Contacts() {
+  if (!accounts || !user) {
     return (
-      <div className="account-filter">
-        <div className="dashboard__contacts">
-          <h4>Contacts</h4>
-          <NavLink to={`/account/${acctId}/contact`}>
-            <button className="btn btn-primary btn-sm btn-icon-text">
-              Add Contact
-            </button>
-          </NavLink>
-          {accountContacts.length > 0 ? (
-            accountContacts.map((contact) => (
-              <div className="dashboard__accounts" key={contact.id}>
-                <p>{contact.name}</p>
-                <p>{contact.position}</p>
-                <p>{contact.phone}</p>
-                <button>Update</button>
-                <button onClick={(e) => handleRemoveContact(e, contact.id)}>
-                  Remove Contact
-                </button>
-              </div>
-            ))
-          ) : (
-            <p>No contacts available</p>
-          )}
-        </div>
+      <div className="dashboard">
+        <strong>PLEASE LOGIN TO CONTINUE</strong>
       </div>
     );
   }
 
-  //  Optimistic UI Update: After the deletion request, the local profile state is immediately updated to remove the deleted action, without waiting for a full refetch.
-  //  This method does not require refetching the entire profile or actions list, making the UI more efficient and responsive.
-  const handleRemoveAction = async (e, actionId) => {
-    e.preventDefault();
-    try {
-      await dispatch(fetchDeleteAction(actionId));
-      setProfile((prevProfile) => ({
-        ...prevProfile,
-        actions: prevProfile.actions.filter((action) => action.id !== actionId),
-      }));
-    } catch (err) {
-      console.error("Failed to delete action:", err);
-    }
-  };
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   function Actions() {
+
     const filterWeekActions = () => {
       const currentTime = new Date();
       const oneWeekFromNow = new Date(currentTime);
@@ -136,32 +62,19 @@ export default function AccountProfile() {
       <div className="account-filter">
         <div className="dashboard__actions">
           <h4>Actions</h4>
-          <NavLink to={`/account/${acctId}/action`}>
-            <button className="btn btn-primary btn-sm btn-icon-text">
-              Add Action
-            </button>
-          </NavLink>
-          <br></br>
           <button onClick={() => setIsWeekFilter(true)}>Weekly</button>
           <button onClick={() => setIsWeekFilter(false)}>Monthly</button>
           {isWeekFilter ? (
             weekActions.length > 0 ? (
               weekActions.map((action) => (
                 <div key={action.id}>
-                  <p>
-                    <strong>Action:</strong> {action.report}
-                  </p>
-                  <br></br>
-                  <p>
-                    <strong>Details:</strong> {action.details}
-                  </p>
-                  <br></br>
-                  <span>
-                    <strong>Due by:</strong> {action.reminder}
-                  </span>
-                  <button onClick={(e) => handleRemoveAction(e, action.id)}>
-                    Remove Action
-                  </button>{" "}
+                  <NavLink to={`/account/${action.accountId}`}>
+                    <p>
+                      <strong>Action:</strong> {action.report}
+                      <br></br>
+                      <span> <strong>Due by:</strong> {action.reminder}</span>
+                    </p>
+                  </NavLink>
                 </div>
               ))
             ) : (
@@ -170,22 +83,15 @@ export default function AccountProfile() {
           ) : monthActions.length > 0 ? (
             monthActions.map((action) => (
               <div key={action.id}>
+              <NavLink to={`/account/${action.accountId}`}>
                 <p>
                   <strong>Action:</strong> {action.report}
+                  <br></br>
+                  <span> <strong>Due by:</strong> {action.reminder}</span>
                 </p>
-                <br></br>
-                <p>
-                  <strong>Details:</strong> {action.details}
-                </p>
-                <br></br>
-                <span>
-                  <strong>Due by:</strong> {action.reminder}
-                </span>
-                <button onClick={(e) => handleRemoveAction(e, action.id)}>
-                  Remove Action
-                </button>{" "}
-              </div>
-            ))
+              </NavLink>
+            </div>
+        ))
           ) : (
             <p>No actions available within the next month.</p>
           )}
@@ -194,91 +100,28 @@ export default function AccountProfile() {
     );
   }
 
-  //  Optimistic UI Update: After the deletion request, the local profile state is immediately updated to remove the deleted action, without waiting for a full refetch.
-  //  This method does not require refetching the entire profile or actions list, making the UI more efficient and responsive.
-  const handleRemoveOrder = async (e, orderId) => {
-    e.preventDefault();
-    try {
-      await dispatch(fetchDeleteOrder(orderId));
-      setProfile((prevProfile) => ({
-        ...prevProfile,
-        orders: prevProfile.orders.filter((order) => order.id !== orderId),
-      }));
-    } catch (err) {
-      console.error("Failed to delete order:", err);
-    }
-  };
-
   return (
     <div className="dashboard">
-      <div className="dashboard__quotes">
-        <span>Quotes</span>
-        <Link to={`/create-order/${acctId}`}>
-          <button className="btn btn-primary btn-sm btn-icon-text">
-            Create Sales Order
-          </button>
-        </Link>
-        {profile && profile.orders && profile.orders.length > 0 ? (
-          profile.orders.map((order, index) => (
-            <div key={index}>
-              <NavLink to={`/sales-order/${order.id}`}>
-                <p>VIN: {order.vin}</p>
+      <div className="dashboard__accounts">
+        <h4>Accounts</h4>
+        {Object.values(accounts).length > 0 ? (
+          Object.values(accounts).map((account) => (
+            <div key={account.id}>
+              <NavLink to={`/account/${account.id}`}>
+                <p>Company Name: {account.companyName}</p>
               </NavLink>
-              <NavLink to={`/account/${acctId}/update-order/${order.id}`}>
-                <button>Update</button>
-              </NavLink>
-              <button onClick={(e) => handleRemoveOrder(e, order.id)}>
-                Delete
-              </button>
             </div>
           ))
         ) : (
-          <p>No quotes available.</p>
+          <p>No accounts available</p>
         )}
       </div>
-      <div className="dashboard__profile">
-        <div>Account Profile</div>
-        <NavLink to={`/account/${acctId}/edit`}>
-          <button className="btn btn-primary btn-sm btn-icon-text">
-            Update Profile
-          </button>
-        </NavLink>
-        {user && profile ? (
-          <>
-            <h2>{profile.companyName}</h2>
-            <p>Vocation: {profile.businessType}</p>
-            <p>Equipment: {profile.equipmentType}</p>
-            <p>Fleet Size: {profile.fleetSize}</p>
-            <p>Looking For: {profile.lookingFor}</p>
-            <p>Phone #: {profile.phoneNumber}</p>
-            <p>Email: {profile.email}</p>
-            <span>{profile.address}</span>
-            <br></br>
-            <span>
-              {profile.city}, {profile.zipCode}
-            </span>
-          </>
-        ) : (
-          <div>Must be logged in</div>
-        )}
+      <div className="account-filter">
+        <FilterAccounts />
       </div>
-      <div>
-        <div className="filter-cards">
-          {Object.values(profile).length > 0 ? (
-            <Actions />
-          ) : (
-            <p>Loading actions...</p>
-          )}
-        </div>
-        <div className="filter-cards">
-          {Object.values(profile).length > 0 ? (
-            <Contacts />
-          ) : (
-            <p>Loading actions...</p>
-          )}
-        </div>
+      <div className="filter-cards">
+        {usersActions.length > 0 ? <Actions /> : <p>Loading actions...</p>}
       </div>
     </div>
   );
 }
-
